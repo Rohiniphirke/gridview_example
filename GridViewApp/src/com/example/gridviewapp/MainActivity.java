50b5d1d5d75c4ab5bdbb213c54fcf9febace5ffa
RohiniPhirke <phirkerohini@gmail.com>
2014-12-24 15:08:00 +0530

    create the constuctor of ImageAdapter
    created handler for gridview in main activity

--- GridViewApp/src/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.gridviewapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
+	
+	GridView gridview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        gridview = (GridView)findViewById(R.id.grid_view);
+        gridview.setAdapter(new ImageAdapter(MainActivity.this));
         
         
         
